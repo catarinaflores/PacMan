4706c1c54ae4774d027d70b9b983264231645a51
catarinaflores <catarina10flores@gmail.com>
2023-12-13 09:13:17 +0000

    level 2 working - missing tile map

---
+++ scenes/menus/main_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _on_level_1_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
+
+
+
+func _on_level_2_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level_2.tscn")
