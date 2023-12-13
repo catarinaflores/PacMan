c75581e9600d472463b2ac18991246cf7d7ba923
catarinaflores <catarina10flores@gmail.com>
2023-12-13 12:55:29 +0000

    finished

--- scenes/menus/main_menu.gd
+++ scenes/menus/main_menu.gd
@@ -4,17 +4,12 @@ func _on_level_1_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn")
 
 
-
 func _on_level_2_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level_2.tscn")
 
 
-
-
-
-
-
-
+func _on_level_3_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level_3.tscn")
 
 
 func _on_quit_button_pressed():
